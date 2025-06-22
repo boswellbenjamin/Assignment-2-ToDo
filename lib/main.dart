3660b116ce040e0bdfabc1f7c36a3c861379d5d4
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-22 14:42:55 +0200

    Remove title and theme from MaterialApp in MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
