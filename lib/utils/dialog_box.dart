ce1ec892f5e204e350794dc46e5b899e7a96b8e4
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-22 16:56:56 +0200

    Refactor DialogBox to include onSave and onCancel callbacks

--- lib/utils/dialog_box.dart
+++ lib/utils/dialog_box.dart
@@ -2,8 +2,17 @@ import 'package:assignment2/utils/my_button.dart';
 import 'package:flutter/material.dart';
 
 class DialogBox extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
   final controller;
-  const DialogBox({super.key, required this.controller});
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +25,21 @@ class DialogBox extends StatelessWidget {
           children: [
             TextField(
               controller: controller,
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(color: Colors.white, fontSize: 18),
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
                 hintText: 'Add a new task',
-                hintStyle: TextStyle(color: Colors.white),
+                hintStyle: TextStyle(
+                  color: const Color.fromARGB(255, 172, 172, 172),
+                  fontSize: 18,
+                ),
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                MyButton(text: 'Save', onPressed: () {}),
+                MyButton(text: 'Save', onPressed: onSave),
 
-                MyButton(text: 'Cancel', onPressed: () {}),
+                MyButton(text: 'Cancel', onPressed: onCancel),
               ],
             ),
           ],
