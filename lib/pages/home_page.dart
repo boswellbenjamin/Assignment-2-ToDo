288399e1bd69a8f05b714cc53355801b6ad4ac26
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-22 15:18:12 +0200

    Update HomePage styling

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,12 +12,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: const Color.fromARGB(255, 20, 20, 20),
       appBar: AppBar(
-        title: Text('My Todo', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+        title: Text(
+          'My Todo',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
       ),
       body: ListView(
         children: [
+          Padding(padding: const EdgeInsets.only(top: 16)),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              'Remaining Tasks (8)',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
           TodoList(),
           TodoList(),
           TodoList(),
