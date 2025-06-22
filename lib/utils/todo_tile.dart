ce2fd0b9b0ff4745b0c7391832ef2b24a8595d63
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-22 15:20:09 +0200

    Refactor TodoList widget to enhance styling and text presentation

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -9,8 +9,15 @@ class TodoList extends StatelessWidget {
       padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
       child: Container(
         padding: const EdgeInsets.all(16),
-        child: Text('Testing todo'),
-        decoration: BoxDecoration(color: Colors.white),
+        decoration: BoxDecoration(color: Color.fromARGB(255, 31, 31, 31)),
+        child: Text(
+          'Testing todo',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
