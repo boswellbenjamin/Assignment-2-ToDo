b50b43766611d80ff5bd23653a397fa0d037eca7
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-22 16:43:32 +0200

    Implement task creation dialog with input field and buttons

--- lib/utils/dialog_box.dart
+++ lib/utils/dialog_box.dart
@@ -1,10 +1,39 @@
+import 'package:assignment2/utils/my_button.dart';
 import 'package:flutter/material.dart';
 
 class DialogBox extends StatelessWidget {
-  const DialogBox({super.key});
+  final controller;
+  const DialogBox({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      backgroundColor: Color.fromARGB(255, 20, 20, 20),
+      content: Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: controller,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Add a new task',
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MyButton(text: 'Save', onPressed: () {}),
+
+                MyButton(text: 'Cancel', onPressed: () {}),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
