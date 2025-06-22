89c63eefa859820afae465e340baabc7f85d4024
Benjamin Boswell <bb222ny@student.lnu.se>
2025-06-22 15:11:46 +0200

    Add basic layout to HomePage with AppBar and TodoList

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:assignment2/utils/todo_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,8 +12,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text('My Todo', style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      body: ListView(
+        children: [
+          TodoList(),
+          TodoList(),
+          TodoList(),
+          TodoList(),
+          TodoList(),
+          TodoList(),
+          TodoList(),
+          TodoList(),
+        ],
       ),
     );
   }
